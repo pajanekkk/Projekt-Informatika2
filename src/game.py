9d4a6e95ca23f4abda7a5a5c442fe38b306022f2
pajanekkk <phovjadsky@gmail.com>
2025-11-20 15:59:14 +0100

    pridani player.py a game.py

--- src/game.py
+++ src/game.py
@@ -1,8 +1,8 @@
+# Game.py slouzi k zakladnim funkcim, ktere umoznuji aby se hra spustila, bezela dobre, napr. vytvoreni okna atd...
+
 import pygame
 from src.player import *
 from src.settings import *
-import random
-import sys
 
 class Game:
     def __init__(self):
@@ -13,12 +13,48 @@ class Game:
 
         self.clock = pygame.time.Clock()
 
+        start_x = WINDOW_WIDTH // 2
+        start_y = WINDOW_HEIGHT - 60
+        self.player = Player(start_x, start_y)
+
+        # do techto poli se budou nahravet vytvorene "entity"
+        self.bullets = []
+        self.enemies = []
+
+        self.score = 0
+        self.lives = PLAYER_LIVES
+
         self.running = True
 
     def run(self):
         while self.running:
+            dt = self._get_delta_time()
+            self._handle_events(dt)
+
             self._draw()
+
+    def _get_delta_time(self):
+        """
+        funkce aby rychlost nepratel + hrace byla konstantne stejna a nebyla zavisla na FPS
+        napr. pohyb 20 pixelu za sekundu a ne 20 pixelu za snimek
+        """
+        dt_ms = self.clock.tick(FPS)
+        dt = dt_ms / 1000.0
+        return dt
     
+    def _handle_events(self, dt):
+        """
+        funkce pro zpracovani eventu, jako je quit, nebo zmacknuti klaves pro strileni atd.
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+        pressed_keys = pygame.key.get_pressed()
+        self.player.handle_input(pressed_keys, dt)
+
     def _draw(self):
-        self.screen.fill((0, 255, 255))
+        self.screen.fill((255, 255, 255))
+
+        self.player.draw(self.screen)
         pygame.display.flip()
---
+++ src/player.py
@@ -0,0 +1,49 @@
+import pygame
+from src.settings import *
+
+PLAYER_COLOR = (0, 255, 0)
+
+class Player:
+    def __init__(self, start_x: float, start_y: float) -> None:
+        """
+        start_x, start_y - pocatecni pozice
+        """
+        self.rect = pygame.Rect(0, 0, PLAYER_WIDTH, PLAYER_HEIGHT)
+        self.rect.centerx = int(start_x)
+        self.rect.centery = int(start_y)
+
+        # rychlost v ose x
+        self.speed = PLAYER_SPEED
+    
+    def handle_input(self, pressed_keys, dt: float) -> None:
+        """
+        Zpracovani pohybu
+        """
+        dx = 0.0 # jakoby osa x
+
+        # posun doleva
+        if pressed_keys[pygame.K_LEFT] or pressed_keys[pygame.K_a]:
+            dx -= self.speed * dt
+
+        # posun doprava
+        if pressed_keys[pygame.K_RIGHT] or pressed_keys[pygame.K_d]:
+            dx += self.speed * dt
+        
+        # posunuti hrace po ose x
+        self.rect.x += int(dx)
+
+        # osetreni hrace mimo obrazovku
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > WINDOW_WIDTH:
+            self.rect.right = WINDOW_WIDTH
+
+    def update(self, dt: float) -> None:
+        # TODO
+        pass
+
+    def draw(self, surface: pygame.Surface) -> None:
+        """
+        Vykresleni hrace na obrazovku
+        """
+        pygame.draw.rect(surface, PLAYER_COLOR, self.rect)
